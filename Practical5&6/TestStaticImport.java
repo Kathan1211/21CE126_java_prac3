ad1e20c26194800ca90d1526a097b45777dafd61
Kathan Shah <96037613+Kathan1211@users.noreply.github.com>
2022-09-17 15:26:25 +0530

    Create TestStaticImport.java

---
+++ Practical5&6/TestStaticImport.java
@@ -0,0 +1,10 @@
+//this is created by 21CE126 Kathan Shah
+//Practical 6
+
+import static P1.UserDefinedPack.sub;
+
+public class TestStaticImport{
+	public static void main(String[] args){
+		sub(10,20);
+	}
+}
