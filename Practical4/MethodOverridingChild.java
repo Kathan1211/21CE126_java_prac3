2cee0114adc8815ce7839baa55c4a5874229a6d2
Kathan Shah <96037613+Kathan1211@users.noreply.github.com>
2022-09-17 15:16:28 +0530

    Create MethodOverridingChild.java

---
+++ Practical4/MethodOverridingChild.java
@@ -0,0 +1,8 @@
+//this is created by 21CE126 Kathan Shah
+public class MethodOverridingChild extends MethodOverridingParent {
+    @Override
+    public void sample() {
+        // TODO Auto-generated method stub
+        System.out.println("In MethodOverridingChild class");;
+    }
+}
