713f9bdb6349ea6f395b256ff459f4fab1621d59
Kathan Shah <96037613+Kathan1211@users.noreply.github.com>
2022-09-17 15:25:34 +0530

    Create Example.java

---
+++ Practical5&6/Example.java
@@ -0,0 +1,12 @@
+//this is created by 21CE126 Kathan Shah
+//Practical 5
+
+import P1.UserDefinedPack;
+
+public class Example{
+	public static void main(String[] args){
+		UserDefinedPack a = new UserDefinedPack();
+		a.getdata();
+		System.out.println("Using a.add():- " + a.add());
+	}
+}
