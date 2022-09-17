d3249d29968d207a94e80b341034c17f24a7c9bb
Kathan Shah <96037613+Kathan1211@users.noreply.github.com>
2022-09-17 15:22:18 +0530

    Create MethodOverrridingParent.java

---
+++ Practical4/MethodOverrridingParent.java
@@ -0,0 +1,7 @@
+//this is created by 21CE126 Kathan Shah
+public class MethodOverridingParent {
+    public void sample()
+    {
+        System.out.println("In MethodOverridingParent class");
+    }
+}
